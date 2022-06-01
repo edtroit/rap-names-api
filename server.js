2782ee9c6018980bd11d06626e33345ecdca6c1f
edtroit <98837125+edtroit@users.noreply.github.com>
2022-05-31 21:52:29 -0400

    we are way over time

---
+++ server.js
@@ -0,0 +1,40 @@
+const { request, response } = require("express")
+
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan':{
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response)=>{
+    const RappersName = request.params.rapperName.toLocaleLowerCase()
+    if(rappers[RappersName]){
+        response.json(rappers[RappersName])
+    }else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
